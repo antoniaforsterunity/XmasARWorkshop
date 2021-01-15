d42b80180e0ae93dfe242bb06decacb6d83e101a
Antonia Forster <antonia.forster@unity3d.com>
2021-01-15 15:54:26 +0000

    Added project to repo
    Project is in working condition - when image markers are in close proximity (actually based on character position, not marker position), Santa should dance and the tree's bauble material changes colour

---
+++ Assets/DanceCommander.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceCommander : MonoBehaviour
+{
+
+    [SerializeField] GameObject tree;
+    [SerializeField] GameObject santa;
+    [SerializeField] Material myMaterial;
+    Animator santaAnimator;
+    
+    
+    void Start()
+    {
+        santaAnimator = santa.GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        float dist = Vector3.Distance(tree.transform.position, santa.transform.position);
+        if(dist < 0.35)
+        {
+            myMaterial.color = Color.yellow;
+            santaAnimator.SetBool("isDancing", true);
+        }
+        else
+        {
+            myMaterial.color = Color.red;
+            santaAnimator.SetBool("isDancing", false);
+        }
+    }
+}
